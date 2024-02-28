97dbb37a76c5a1ad38bcc5cdb7c069abdd9060c5
Abhay-1552 <patelabhay1552@gmail.com>
2024-02-28 12:46:24 +0530

    Working

--- Client-1/client.py
+++ Client-1/client.py
@@ -2,27 +2,28 @@ import socket
 import threading
 
 
-# Function to receive messages from the server
-def receive_messages(sock):
-    while True:
-        try:
-            message = sock.recv(1024).decode("utf-8")
-            print(message)
-        except OSError:  # Client disconnected
-            break
+class CLIENT:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(('localhost', 9999))
+
+        receive_thread = threading.Thread(target=self.receive_messages)
+        receive_thread.start()
+
+    def receive_messages(self):
+        while True:
+            try:
+                message = self.client.recv(1024).decode("utf-8")
+                print(message)
+                return message
+            except Exception as e:
+                print(f"Error: {e}")
+                return f"Error: {e}"
+
+    def send_messages(self, message):
+        self.client.send(message.encode("utf-8"))
 
 
-# Create a socket
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Connect to the server
-client.connect(('localhost', 9999))
-
-# Start a thread to receive messages from the server
-receive_thread = threading.Thread(target=receive_messages, args=(client,))
-receive_thread.start()
-
-# Send messages to the server
-while True:
-    message = input()
-    client.send(message.encode("utf-8"))
+# if __name__ == '__main__':
+#     client = CLIENT()
+#     client.send_messages()
---
+++ Client-1/index.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from client import CLIENT
+
+app = Flask(__name__, template_folder='template')
+
+client = CLIENT()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route("/message", methods=["POST"])
+def message():
+    if request.method == "POST":
+        chat = request.form.get("messageInput")
+        client.send_messages(chat)
+        return '', 204
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
