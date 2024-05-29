d93d0bd220fdf97d2e5eab7222d2485a1c2e2c32
Abhay-1552 <patelabhay1552@gmail.com>
2024-05-29 16:00:48 +0530

    Working

---
+++ Client-2/client.py
@@ -0,0 +1,24 @@
+import socket
+import threading
+
+
+class CLIENT:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(('localhost', 9999))
+
+        receive_thread = threading.Thread(target=self.receive_messages)
+        receive_thread.start()
+
+    def receive_messages(self):
+        while True:
+            try:
+                message = self.client.recv(1024).decode("utf-8")
+                print(message)
+                return message
+            except Exception as e:
+                print(f"Error: {e}")
+                return f"Error: {e}"
+
+    def send_messages(self, message):
+        self.client.send(message.encode("utf-8"))
--- Client-2/index.py
+++ Client-2/index.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 from client import CLIENT
-import asyncio
 
 app = Flask(__name__, template_folder='template')
 
